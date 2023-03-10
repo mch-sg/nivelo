fdf501d746fdfd65db0b1feee272e7f9135b7239
mch-sg <magnus.hvidtfeldt@gmail.com>
2023-03-10 22:55:18 +0100

    db updates

--- db/includes/dbh.inc.php
+++ db/includes/dbh.inc.php
@@ -1,12 +1,24 @@
 <?php
 
 $serverName = "127.0.0.1:3306";
-$dBUsername = "u463909974_login";
-$dBPassword = "Jsu78khv";
-$dBName = "u463909974_demo";
+$dBUsername = "u463909974_exam";
+$dBPassword = "Ekg123321";
+$dBName = "u463909974_portal";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
     die("Connection failed: ".mysqli_connect_error());
 }
+
+
+
+/*
+old
+
+$serverName = "127.0.0.1:3306";
+$dBUsername = "u463909974_login";
+$dBPassword = "Jsu78khv";
+$dBName = "u463909974_demo";
+
+*/
